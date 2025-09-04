266c105da9bc885de3b42d106d2b0937108bcc32
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-04 11:24:30 +0530

    Non-Primitive data types

--- DataType_NonPrim.java
+++ DataType_NonPrim.java
@@ -20,15 +20,20 @@ public class DataType_NonPrim {
 	 
 	 // Arrays  --> Collection of similar data type, also linear data structure.
 	 
-	 int[] marks = new int[5]; // it size of 5 but it start with 0 index
+	 int[] marks = new int[5];         // created object(new keyword)for array
+	// it size of 5 but it start with 0 index
+   // indexes   values	 
 	 marks[0] = 54;
 	 marks[1] = 76;
 	 marks[2] = 87;
 	 marks[3] = 98;
 	 marks[4] = 12;
-	 System.out.println(marks[3]);
+	 System.out.println(marks.length); // display the length
 	 
 	 
+	 String add = "pune";
+	 System.out.println(12+12+add+3+4+10); // JVM work left to right
+	 // when it countered the string it get concatenate the other values or num.
 	}
 
 
