2bd43fc30db38aae10fd5a27ec5405ce797f84b1
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-07 15:32:40 +0530

    Operators in detail.

--- Operators.java
+++ Operators.java
@@ -5,6 +5,7 @@ public class Operators {
 	public static void main(String[] args) {
 		
 		/* Operators are used to perform operations on variables and values
+		  Always add only two variable or value at a time.
 		  
 
 		   Java divided the operators into following groups::
@@ -21,7 +22,43 @@ public class Operators {
 			6) Ternary Operator      =>     condition ? value_if_true : value_if_false
 
 	   			*/
-		System.out.println("Its a binary operators.");
+		System.out.println("Its a binary operators."); 
+		
+// *1) Arithmetic operator :: it works on  mathematical operations.
+		
+		int a = 5, b = 8,z,x,y,w;
+		 z = a+b;
+		 x = a-b;
+		 y = a*b;
+		 w = a/b;
+		float v = a%b;
+		System.out.println("************ Arithmetic operators *********");
+		System.out.println("Addition ::  " + z + "\nSubstraction is :: " + x + "\nMultiplication is :: " + y + "\nSubtraction is :: " + w + "\nModulus is :: " +v);
+		 
+		//increment and decrement =>  unary operators ++ and -- used to increase and decrease a value by 1. 
+		// increment ++  :: pre and post 
+		
+		int num = 5;
+		num++;  //5		 Post => increase later :: first uses the value, then increases it 
+		++num;  //7	 	 Pre  => increase first :: first increases the value, then uses it.
+		num++;  //7
+		
+	//                      8      8         10        10	
+		System.out.println(num+ (num++) + (++num) + (num++));
+		System.out.println(num); // 11
+		
+		// decrement --  :: pre and post
+		
+		int no = 12;
+		no--;  //12		post => decrease later :: first uses the value then decrease it.
+		--no;  //10		pre  => decrease first :: first decrease the value, then uses it.
+		no--;  //10
+	// 	                   9      8        8        7        5
+		System.out.println(no + (--no) + (no--) + (no--) + (--no));
+		System.out.println(no); // 5
+		
+		
+		
 	}
 
 }
