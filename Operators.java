a0a8708ed64f4793a5fdf98105e4519238df868b
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-08 16:21:09 +0530

    Operators in details

--- Operators.java
+++ Operators.java
@@ -11,7 +11,7 @@ public class Operators {
 		   Java divided the operators into following groups::
 			1) Arithmetic Operator   => 	+,-,*,/,%,++,-- 
 			
-	2) Relational/Comparison Operators =>    ==, !=, >, <, >=, <=
+	2) Relational/Comparison Operators =>    ==, !=, >, <, >=, <=          
 	
 			3) Assignment Operators  =>		=,+=,-=,*=,/=,%=,&=,|=,^=,>>=,<<=
 			
@@ -56,6 +56,32 @@ public class Operators {
 	// 	                   9      8        8        7        5
 		System.out.println(no + (--no) + (no--) + (no--) + (--no));
 		System.out.println(no); // 5
+		
+//*2) Assignment Operator :: it used to assign a value to any variable.		
+		
+		int ab = 23; // = is a assignment operator to assign the value to a variable called ab.
+		
+		  ab += 5; // short-hand statement  ab = ab + 5; we can use many cases.
+		  System.out.println(ab);
+		
+		int as = 4;
+		System.out.println("***Assignment Operators***  " +as);
+		System.out.println("as+=3 :: " + (as+=3));
+		System.out.println("as-=2 :: " + (as-=2));
+		System.out.println("as*=5 :: " + (as*=5));
+		System.out.println("as/=4 :: " + (as/=4));
+		System.out.println("as%=4 :: " + (as%=4));
+		
+//*3) Relational Operator :: used to checked for relations like equality,greater than and less than.
+		//                     they return boolean results after the comparison
+		int d =5, f =8, g = 5;
+		System.out.println("***Relational Operators***");
+		System.out.println("d > f : " + (d > f));
+		System.out.println("f < g : " + (f < g));
+		System.out.println("d <= g: " + (d <= g));
+		System.out.println("f >= d: " + (f >= d));
+		System.out.println("g == f: " + (g == f));
+		System.out.println("f != d: " + (f != d));
 		
 		
 		
