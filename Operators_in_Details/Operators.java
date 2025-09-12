e3464a2ea1f21fa0f7e856237ceedf326afb6b7b
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-12 14:43:49 +0530

    Operators in details

--- Operators_in_Details/Operators.java
+++ Operators_in_Details/Operators.java
@@ -6,12 +6,13 @@ public class Operators {
 		
 		/* Operators are used to perform operations on variables and values
 	   Java divided the operators into following groups::
-		1) Arithmetic Operator   => 	+,-,*,/,%,++,-- 
+		1) Arithmetic Operator   => 	+,-,*,/,%
 2) Relational/Comparison Operators =>    ==, !=, >, <, >=, <=
 		3) Assignment Operators  =>		=,+=,-=,*=,/=,%=,&=,|=,^=,>>=,<<=
 		4) Logical Operators	 => 	&&,||,!
-		5) Bitwise Operators	 => 	AND(&),OR(|),XOR(^),NOT(~),Left Shift(<<),Right Shift(>>)
-		6) Ternary Operator      =>     condition ? value_if_true : value_if_false
+		5) Unary Operators => +, -, ++, --, !
+		6) Bitwise Operators	 => 	AND(&),OR(|),XOR(^),NOT(~),Left Shift(<<),Right Shift(>>)
+		7) Ternary Operator      =>     condition ? value_if_true : value_if_false
 
    			*/
 		System.out.println("Hello");
