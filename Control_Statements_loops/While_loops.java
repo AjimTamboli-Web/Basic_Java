29fff719d38c327977aa915dbeaa810322f311d7
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-17 21:01:02 +0530

    While Loops

---
+++ Control_Statements_loops/While_loops.java
@@ -0,0 +1,18 @@
+package java_plane.Control_Statements_loops;
+
+public class While_loops {
+
+	public static void main(String[] args) {
+		//Uses when number of iterations is Unknown.
+		//Used when the number of iterations is not known in advance, condition checked before each iteration.
+	//Also use for when we want to check the condition before executing the loop body
+	// also called Entry Control Loop	
+		int i = 0;
+		while(i<10) {
+			System.out.println(i);
+			i++;
+		}
+
+	}
+
+}
