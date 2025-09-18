745baba046d24a67e436c5dbd349a454984b1358
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-18 16:35:01 +0530

    Loops in Details

--- Loops.java
+++ Loops.java
@@ -25,7 +25,38 @@ public class Loops {
 			System.out.println("For Loop" + i); //B
 		}
 		
-
+/* 2)While loop :->
+		Uses when number of iterations is Unknown.
+	    Used when the number of iterations is not known in advance, condition checked before each iteration.
+		Also use for when we want to check the condition before executing the loop body
+		also called Entry Control Loop	*/
+		
+		int i = 0;      			   // Initialization I
+		while(i<5) {    		       // Condition      C
+			System.out.println(i);     // Block of code  B
+			i++;                       // Operation      O
+		}
+		
+/* 3)Do While loop :->
+        Similar to while loop, but condition is checked after executing the block (executes at least once).
+  		The do-while loop ensures that the code block executes at least once before checking the condition.
+  		The do/while loop is a variant of the while loop. This loop will execute the code block once, before checking if the condition is true. 
+  		then it will repeat the loop as long as the condition is true.
+ */
+		
+		int s = 0;                 // Initialization I
+		do {                       // Block of code  B
+			System.out.println(s);
+			s++;                   // Operation      O
+		}while(s<5);               // Condition      c
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
 
