ca6cfbfb76fd8b612c4195ad9f25804da225ef7e
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-27 21:21:32 +0530

    Demo

--- Demo.java
+++ Demo.java
@@ -1,10 +1,28 @@
 package java_plane;
 
+
 public class Demo {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World \n daek \n sefdsf" );
+		System.out.println(" Hello World \n This is \n Demo" );
+		
+		int i = 3543;
+		int ori = i;
+		int rev = 0;
+		
+		while(ori > 0) {
+			int d = ori % 10;
+			rev = rev * 10 + d;
+			ori = ori / 10;
+		}
+		if(i == rev) {
+	      System.out.println("It's Palindome");
+		}
+		else {
+			System.out.println("it's not");
+		}
+		
 		
 	}
 
