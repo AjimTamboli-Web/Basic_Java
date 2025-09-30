fe0af0c7f111cc4a64b1a2332f0017e516179cf4
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-09-30 16:13:00 +0530

    Classes and objects

--- Class_Object.java
+++ Class_Object.java
@@ -1,5 +1,15 @@
 package java_plane;
 
+/*
+ * classes and objects are basic concepts of Object Oriented Programming (OOPs) that are used to represent real-world concepts and entities.
+ * Class ->A class is a template to create objects having similar properties and behavior, or in other words, we can say that a class is a blueprint for objects.
+ * Object -> An object is an instance of a class.  You can create multiple objects of one class:
+ * 
+ * 
+ */
+
+
+
 public class Class_Object {
 
 	public static void main(String[] args) {
@@ -7,5 +17,4 @@ public class Class_Object {
 		
 		
 	}
-
 }
