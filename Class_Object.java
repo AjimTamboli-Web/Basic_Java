622bde3734fa8b5d498dad4145358e9fcba3408a
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-01 14:45:10 +0530

    Classes and Objects

--- Class_Object.java
+++ Class_Object.java
@@ -14,7 +14,30 @@ public class Class_Object {
 
 	public static void main(String[] args) {
 		
+		Car Honda = new Car();	
+		Honda.model="T20";
 		
+		Car Ferrari = new Car();
+		Ferrari.model = "A7000";
 		
+		Honda.carRun();
+		Ferrari.carStop();
+	}
+}
+
+
+class Car{
+	String model;
+	String Name;
+	double price;
+	
+	public String carRun() {
+		String model = "Model: ";
+		System.out.println("Car is running.......");
+		return model;
+	}
+	
+	void carStop() {
+		System.out.println("Car is park in parking........");
 	}
 }
