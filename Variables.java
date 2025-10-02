03a1856eec61a1c23d791428c3406bf49fa2182f
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-02 17:35:27 +0530

    Variables

--- Variables.java
+++ Variables.java
@@ -3,22 +3,35 @@ package java_plane;
 /*
  * A variables is like a container(memory location) used to store data, it has a name,data type and a value.
  * Variable type : instance , local , static
- * Instance variable -> Declare inside class but outside methods\constructor\block, it specific to objects.
+ 
+ * Instance variable / object level variables / Attributes : 
+ *               -> If the value of a variable is varied from Object to object such type of variables are called instance variables.
+  					 -  Every object separate copy of instance variables will be created.
+  					 -  Declare inside class but outside methods\constructor\block, it specific to objects.
                      -  Belongs to an object (Instance) of the class.
                      -  Each object has its own copy of instance variables.
                      -  Stored in heap memory (since they are part of the objects).
                      -  it has a default values.assigned automatically 0 for int, null for objects, false for boolean.
                      -  scope is whole class, access in anywhere in whole class.
- * local variable -> Declare inside methods/ constructor/ blocks and used only there.
-                  -  it does not have default value, must be initialized before use.
-                  -  it can access only in method itself.
-                  -  Stored in stack memory. 
-                  -  Exist only during the execution of that method.             
- * static variable -> Declare with static keyword, shared across all objects.
-                   -  Belongs to the class, not to any object.
-                   -  All objects share one copy of a static variable.
-                   -  Stored in Method area (class area) of memory.
-                   -  Can be accessed using class name or object name.
+                     -  we can't access instance variable directly from static area but we can access by using object reference,
+                            but we can access instance variable directly from instance area. 
+                     
+ * local variable  -> Declare inside methods/ constructor/ blocks and used only there.
+                     -  it does not have default value, must be initialized before use.
+                     -  it can access only in method itself.
+                     -  Stored in stack memory. 
+                     -  Exist only during the execution of that method.
+                               
+ * static variable / class level variables / Fields :
+                     -  Static variables should be declared with in the class directly but outside of any block / constructor / method.                   
+                     -  Declare with static keyword, shared across all objects.
+                     -  Belongs to the class, not to any object.
+                     -  All objects share one copy of a static variable.
+                     -  Stored in Method area (class area) of memory.
+                     -  Can be accessed using class name or object name, but recommended to access class name.
+                     -  Scope of static variable is exactly same as scope of .class file.
+                     -  Access static variable directly from both instance and static areas.
+                     -  JVM will provide default values and we are not required to perform initialization explicitly.
  */
 
 
@@ -36,7 +49,7 @@ public class Variables {
 		Var.company = "Infosys";
 		obj.myMethod();
 		obj.myMeth();
-		obj.myMeth();
+		Var.myMeth();
 		
 			Var obj2 = new Var();
 			obj2.age = 45;
