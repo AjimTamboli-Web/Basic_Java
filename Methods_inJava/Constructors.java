3d22b0659d6ca58338492bfaaad42ddf33a5f892
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-08 14:35:31 +0530

    Constructors intro

--- Methods_inJava/Constructors.java
+++ Methods_inJava/Constructors.java
@@ -1,6 +1,7 @@
 package java_plane.Methods_inJava;
 
-/*
+
+/*   Special kind of methods, there is no return type in constructors.
  *   Constructors play an important role in object creation.
  *   A constructors is a special block of code that is called when an object is created. job is initialized object.
  *   to setup its internal state, to assign default values to its attributes.
@@ -9,49 +10,52 @@ package java_plane.Methods_inJava;
  *   A constructor in Java can not be abstract, final, static, or Synchronized.
  *   Access modifiers can be used in constructor declaration to control its access i.e which other class can call the constructor.
  *   Constructors can also take parameters, which is used to initialize attributes.
- *   Types of constructors =>> a> default constructors b> parameterized constructors c> copy constructors
- *   
+ *   Types of constructors =>> a> default constructors 
+    						   b> parameterized constructors 
+    						   c> copy constructors
+    
  */
 
 public class Constructors {
 
 	public static void main(String[] args) {
+		
 	
-		Con cons = new Con();   // constructors create by default when object is created.
+		Const cons = new Const();   // constructors create by default when object is created.  A default constructor is invisible.
 		
-		cons.a = 344;
-		System.out.println("Hello" + cons);
-		
-		Con con1 = new Con(32);  // parameterized constructors and int is parameter is passing
-		
-		con1.a = 432;
-		System.out.println(con1);
-		
-		cons.add(23);
+		cons.a= 123;
 		
 		
-		cons.equals(con1);
+		Const con1 = new Const(32);  // parameterized constructors and int in parameter is passing
+		
+			con1.name= "Hello";	
+
 	}
 
 }
 
-class Con {
+class Const {
 	
 	int a = 34;
+	String name = "Hii";
 	
-	public Con() {
-		System.out.println("Hello");
-		// Assume default constructors when you don't mention
+	public Const() {
+		System.out.println("Hello 1");
+		// Assume default constructors when you don't mention . A default constructor is invisible.
+		display();
 	}
 	
-	public Con(int a) {             // parameterized constructors
-		System.out.println("Hello");
+	
+	public void display() {
+		System.out.println("this is method and executing in constructor. ");
+	}
+	
+	public Const(int a) {             // parameterized constructors
+		System.out.println("Hello 2");
 		a = 34;
 		
 	}
 	
-	public static void add(int... x) {
-		System.out.println("Var-arg method");
-	}
+	
 	
 }
