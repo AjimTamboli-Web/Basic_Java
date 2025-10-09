d165ad6d4360d6571710ad7d332d24f48453e1f3
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-09 20:52:24 +0530

    String Operations

---
+++ Strings_Operations.java
@@ -0,0 +1,19 @@
+package java_plane;
+
+/*
+ * A String is a sequence of characters used to store and manipulate text.Strings are immutable, meaning once created, they cannot be changed.
+ * You can create a string using string literal or new keyword.
+ * Operations - Concatenation - join two strings, Length - Finds string length, CharacterAt - Gets characters at position, Compare .equals - compare string values
+ *               Replace - replaces characters, toUpperCase - converts to capital letters  
+ */
+
+public class Strings_Operations {
+
+	public static void main(String[] args) {
+		
+		String name = "dfgghghjhgd ghgh fghgfhd ghf";
+		System.out.println(name.);
+		
+	}
+
+}
