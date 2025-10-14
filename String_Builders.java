84cf8cdd083503f889c40dc2b752927e5d13ff38
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-14 15:17:47 +0530

    String Builders

--- String_Builders.java
+++ String_Builders.java
@@ -14,8 +14,6 @@ package java_plane;
 public class String_Builders {
 
 	public static void main(String[] args) {
-
-	
 		
 		StringBuilder sb = new StringBuilder("Welcome to ");
 		     sb.append("Java World");  // content of sb changed --- no new object was created.
@@ -25,6 +23,28 @@ public class String_Builders {
          
          StringBuilder sb2 = new StringBuilder(str);  // convert String to StringBuilder
          System.out.println(sb2);
+         
+     System.out.println("**********************************************");   
+     
+         String op1 = "abc";
+         String op2 =  op1.concat("def");  // it use new keyword internally, does not change original value that's why it's immutable
+         
+         StringBuilder op3 = new StringBuilder(op1);  
+         StringBuilder op4 = op3.append("def");   // change original value that's why it's mutable
+                       
+          
+         System.out.println(op1);  // does not change original value
+         System.out.println(op2);  // print in new string variable for concatenate 
+         System.out.println("*************");
+         System.out.println(op3);  // change value at line no 33
+         System.out.println(op4);  // use the old variable value to concatenate without creating object
+         System.out.println("*************");
+         System.out.println(op1 == op2);   // different reference - string
+         System.out.println(op3 == op4);   // same reference      - stringBuilder
+         System.out.println("*************");
+         System.out.println(op1.equals(op2));   // false - different values
+         System.out.println(op3.equals(op4));   // true - same values
+         
 	}
 
 }
