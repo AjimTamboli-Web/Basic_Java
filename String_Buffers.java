364bbb1768c43c205fa63f15783ae41b757314b6
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-14 21:18:34 +0530

    String Buffers overview

--- String_Buffers.java
+++ String_Buffers.java
@@ -1,9 +1,15 @@
 package java_plane;
 
+/*
+ * 
+ */
+
 public class String_Buffers {
 
 	public static void main(String[] args) {
 		
+		String ab = "adgs";
+		StringBuffer sb = new StringBuffer(ab);
 		
 
 	}
