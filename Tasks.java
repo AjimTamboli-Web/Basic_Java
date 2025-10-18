4c76f6daf717fcb4724f012173752c682a2c3e2c
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-18 10:07:13 +0530

    Swap Numbers

--- Tasks.java
+++ Tasks.java
@@ -9,4 +9,28 @@ public class Tasks {
 	}
 }
 
-
+class Task{
+	 // Using + and - operator
+	public static void swap(int No1,int No2) {
+		int a = No1;
+		int b = No2;
+		System.out.println("Before Swap: " + a + " & " + b);
+		a = a + b; 
+		b = a - b;  
+		a = a - b;  
+		System.out.println("After Swap: " + a + " & " + b);
+	}
+	
+	// Using * and / operator
+	public static void swap2(int No1,int No2) {
+		
+		int a = No1;
+		int b = No2;
+		System.out.println("Before Swap: " + a + " & " + b);
+		a = a * b;  
+		b = a / b;  
+		a = a / b; 
+		System.out.println("After Swap: " + a + " & " + b);
+		
+	}
+}
