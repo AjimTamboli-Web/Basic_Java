e54ecc017a4e0d193f9ae4cb8c1de79fc58978f5
AjimTamboli-Web <ajimtamboli313@gmail.com>
2025-10-22 08:28:42 +0530

    Rough work swap no

--- Rough.java
+++ Rough.java
@@ -4,8 +4,24 @@ public class Rough {
 
 	public static void main(String[] args) {
 		
+	int no = 98756345;
+	int og = no;
+	int digiLen = String.valueOf(no).length();    
+	int first = 0;
+	int last = no % 10;
 	
-		
+	while(no > 0) {
+		first = no;
+		 no = no / 10;
+	}
+	System.out.println(digiLen);
+	
+	int middle = (og % (int)Math.pow(10 , digiLen - 1)) / 10 ;
+	
+	int swap = (last * (int)Math.pow(10, digiLen - 1)) + (middle * 10) + first;
+	
+	System.out.println(middle);
+	System.out.println(swap);
 	}
 
 }
